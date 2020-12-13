d488d5af640075f8b1e1eb9a962a85024eb4cec4
emazdraveska <73293769+emazdraveska@users.noreply.github.com>
2020-12-13 20:10:06 +0100

    Application Code

---
+++ Homework3/dians_parking/src/main/java/mk/ukim/finki/dians_parking/DiansParkingApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.dians_parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiansParkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiansParkingApplication.class, args);
+    }
+
+}
