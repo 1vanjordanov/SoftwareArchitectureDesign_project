afeb7326aec5d44d7e41b8728b5400679d026998
emazdraveska <73293769+emazdraveska@users.noreply.github.com>
2020-12-19 20:50:35 +0100

    Update ParkingApplication.java

--- Homework3/parking_application/src/main/java/mk/ukim/finki/dians/parking_application/ParkingApplication.java
+++ Homework3/parking_application/src/main/java/mk/ukim/finki/dians/parking_application/ParkingApplication.java
@@ -2,6 +2,9 @@ package mk.ukim.finki.dians.parking_application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ParkingApplication {
@@ -9,5 +12,8 @@ public class ParkingApplication {
     public static void main(String[] args) {
         SpringApplication.run(ParkingApplication.class, args);
     }
-
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
 }
