2b895979c83b9fd904c3e6b9e2b37048095527c2
emazdraveska <73293769+emazdraveska@users.noreply.github.com>
2020-12-19 20:49:59 +0100

    Create ParkingService.java

---
+++ Homework3/parking_application/src/main/java/mk/ukim/finki/dians/parking_application/service/ParkingService.java
@@ -0,0 +1,45 @@
+package mk.ukim.finki.dians.parking_application.service;
+
+import mk.ukim.finki.dians.parking_application.model.Parking;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ParkingService {
+    List<Parking> findAll();
+
+    Optional<Parking> findById(Long id);
+
+    Optional<Parking> findByName(String name);
+
+    void deleteById(Long id);
+
+    Optional<Parking> findByRating(double rating);
+
+    Optional<Parking> findByRatingGreaterThanEqual(double rating);
+
+    Optional<Parking> findByRatingLessThanEqual(double rating);
+
+    Optional<Parking> findTop5ByRating(double rating);
+
+    List<Parking> findByLocationAddress(String text);
+
+    List<Parking> findByLocationCity(String text);
+
+    List<Parking> findByLocationAddressInCity(String address, String city);
+
+    List<Parking> findByCurrentAddress(Double currentlat, Double currentlng);
+
+    List<Parking> findAllByCityOrderByRatingDesc(String city);
+
+    List<Parking> findAllByAddressOrderByRatingDesc(String address);
+
+    List<Parking> findAllByAddressAndCityOrderByRatingDesc(String address, String city);
+
+    List<Parking> findAllByCityOrderByName(String city);
+
+    List<Parking> findAllByAddressOrderByName(String address);
+
+    List<Parking> findAllByAddressAndCityOrderByName(String address, String city);
+
+}
