c6d66a22c0910ef5d216fb37f6e6df252e54a46e
1vanjordanov <ivan.bazinga@protonmail.com>
2020-12-19 19:47:16 +0100

    Create Parking.java

---
+++ Homework3/parking_application/src/main/java/mk/ukim/finki/dians/parking_application/model/Parking.java
@@ -0,0 +1,36 @@
+package mk.ukim.finki.dians.parking_application.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "parkings")
+public class Parking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String city;
+    private String address;
+    private Double latitude;
+    private Double longitude;
+    private String rating;
+
+    public Parking() {
+
+    }
+
+    public Parking(String name, String city, String address, Double latitude, Double longitude, String rating) {
+        this.name = name;
+        this.city = city;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.rating = rating;
+    }
+
+
+}
