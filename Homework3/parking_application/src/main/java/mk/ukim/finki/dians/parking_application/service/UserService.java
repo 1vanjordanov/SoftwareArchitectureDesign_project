4d3fdb1503ecb0e122a1a7f8d7d047d7c2fa3777
emazdraveska <73293769+emazdraveska@users.noreply.github.com>
2020-12-19 20:49:45 +0100

    Create UserService.java

---
+++ Homework3/parking_application/src/main/java/mk/ukim/finki/dians/parking_application/service/UserService.java
@@ -0,0 +1,9 @@
+package mk.ukim.finki.dians.parking_application.service;
+
+import mk.ukim.finki.dians.parking_application.model.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+public interface UserService extends UserDetailsService {
+    User register(String username, String password, String repeatPassword, String name, String surname);
+
+}
