9ed211f151ba53d7f3f3e471d6b6aa850201a4c0
emazdraveska <73293769+emazdraveska@users.noreply.github.com>
2021-01-26 17:08:59 +0100

    Update LoginController.java

--- Homework3/parking_application/src/main/java/mk/ukim/finki/dians/parking_application/web/controller/LoginController.java
+++ Homework3/parking_application/src/main/java/mk/ukim/finki/dians/parking_application/web/controller/LoginController.java
@@ -5,6 +5,7 @@ import mk.ukim.finki.dians.parking_application.model.exceptions.InvalidUserCrede
 import mk.ukim.finki.dians.parking_application.service.AuthService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -15,31 +16,19 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/login")
 public class LoginController {
 
-    private final AuthService authService;
+    @GetMapping
+    public String getLoginPage(Model model) {
 
-    public LoginController(AuthService authService) {
-        this.authService = authService;
+        model.addAttribute("bodyContent", "login");
+        return "master-template";
     }
 
-    //@GetMapping
-    @RequestMapping(method = RequestMethod.GET, value = "")
-    public String getLoginPage() {
-        return "login";
-    }
+    @GetMapping("/error")
+    public String getLoginPage(Model model, InvalidUserCredentialsException exception) {
 
-    @PostMapping
-    public String login(HttpServletRequest request, Model model) {
-        User user = null;
-        try {
-
-            user = this.authService.login(request.getParameter("username"),
-                    request.getParameter("password"));
-            request.getSession().setAttribute("user", user);
-            return "redirect:/";
-        } catch (InvalidUserCredentialsException exception) {
-            model.addAttribute("hasError", true);
-            model.addAttribute("error", exception.getMessage());
-            return "login";
-        }
+        model.addAttribute("hasError", true);
+        model.addAttribute("error", exception.getMessage());
+        model.addAttribute("bodyContent", "login");
+        return "master-template";
     }
 }
