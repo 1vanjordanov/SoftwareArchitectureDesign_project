81bc4abb1d071ff5d930a2c50d7edc7c40e0f6bb
emazdraveska <73293769+emazdraveska@users.noreply.github.com>
2021-01-26 19:02:45 +0100

    Update login error

--- Homework3/parking_application/src/main/java/mk/ukim/finki/dians/parking_application/web/controller/HomeController.java
+++ Homework3/parking_application/src/main/java/mk/ukim/finki/dians/parking_application/web/controller/HomeController.java
@@ -1,5 +1,6 @@
 package mk.ukim.finki.dians.parking_application.web.controller;
 
+import mk.ukim.finki.dians.parking_application.model.exceptions.InvalidUserCredentialsException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
     @GetMapping
     public String getHomePage(Model model) {
         model.addAttribute("bodyContent", "home");
--- Homework3/parking_application/src/main/java/mk/ukim/finki/dians/parking_application/web/controller/LoginController.java
+++ Homework3/parking_application/src/main/java/mk/ukim/finki/dians/parking_application/web/controller/LoginController.java
@@ -5,6 +5,7 @@ import mk.ukim.finki.dians.parking_application.model.exceptions.InvalidUserCrede
 import mk.ukim.finki.dians.parking_application.service.AuthService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -43,5 +44,14 @@ public class LoginController {
             model.addAttribute("bodyContent", "login");
             return "master-template";
         }
+    }
+
+    @GetMapping("/error")
+    public String getLoginPage(Model model, InvalidUserCredentialsException exception) {
+
+        model.addAttribute("hasError", true);
+        model.addAttribute("error", exception.getMessage());
+        model.addAttribute("bodyContent", "login");
+        return "master-template";
     }
 }
